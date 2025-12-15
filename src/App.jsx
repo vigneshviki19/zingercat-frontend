619aad2a50ee0f11bb8c948b4518bfc333fb7614
vigneshviki19 <vigneshvengatesh07@gmail.com>
2025-12-15 22:14:54 +0530

    Initialize App component with routing setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Feed from "./pages/Feed";
+import CreatePost from "./pages/CreatePost";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/create" element={<CreatePost />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
