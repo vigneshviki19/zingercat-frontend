45b6283151bbd87bf24800f505877a08606399df
vigneshviki19 <vigneshvengatesh07@gmail.com>
2025-12-15 22:07:43 +0530

    Implement basic login functionality in Login component

---
+++ src/pages/Login.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import api from "../api";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    const res = await api.post("/auth/login", { email, password });
+    alert("Welcome " + res.data.username);
+    localStorage.setItem("token", res.data.token);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
+      <input placeholder="Password" type="password" onChange={e => setPassword(e.target.value)} />
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
