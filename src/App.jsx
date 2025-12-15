b43bebc73a85c63a74d95f95fd2e7bb31a1a0b40
vigneshviki19 <vigneshvengatesh07@gmail.com>
2025-12-15 22:28:19 +0530

    Add Navbar component to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Feed from "./pages/Feed";
@@ -7,6 +8,7 @@ import CreatePost from "./pages/CreatePost";
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Feed />} />
         <Route path="/login" element={<Login />} />
