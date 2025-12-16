60cb4045be600cc6b82cc7bd9c469892f8e8e48d
vigneshviki19 <vigneshvengatesh07@gmail.com>
2025-12-16 11:34:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,3 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Landing from "./pages/Landing";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 import Home from "./pages/Home";
-import CreatePost from "./pages/CreatePost";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* Landing page */}
-        <Route path="/" element={<Landing />} />
-
-        {/* Auth pages */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-
-        {/* Main app (after login) */}
-        <Route path="/home" element={<Home />} />
-        <Route path="/create" element={<CreatePost />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+<Route path="/home" element={<Home />} />
