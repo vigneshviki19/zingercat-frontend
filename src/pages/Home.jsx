310a4e4e607ac69c21a8467ae27fc1a345789f49
vigneshviki19 <vigneshvengatesh07@gmail.com>
2025-12-16 11:31:10 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,5 @@
 import Feed from "./Feed";
 
 export default function Home() {
-  const username = localStorage.getItem("username");
-
-  return (
-    <div style={container}>
-      <div style={header}>
-        <h2>🐱 Zinger Cat</h2>
-        <p style={{ fontSize: 14, color: "#555" }}>
-          Welcome back, <b>{username}</b>
-        </p>
-      </div>
-
-      {/* Feed with textarea */}
-      <Feed />
-    </div>
-  );
+  return <Feed />;
 }
