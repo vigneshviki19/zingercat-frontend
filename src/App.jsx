826fb8dbbe9c60e227b3220682cf2f8fefde9d0b
vigneshviki19 <vigneshvengatesh07@gmail.com>
2025-12-20 23:50:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,17 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
-import Landing from "./pages/Landing";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Home from "./pages/Home";
 
 export default function App() {
-  const isLoggedIn = !!localStorage.getItem("token");
-
   return (
     <BrowserRouter>
       <Routes>
-        {/* Landing page */}
-        <Route path="/" element={<Landing />} />
-
-        {/* Auth pages */}
-        <Route
-          path="/login"
-          element={isLoggedIn ? <Navigate to="/home" /> : <Login />}
-        />
-        <Route
-          path="/register"
-          element={isLoggedIn ? <Navigate to="/home" /> : <Register />}
-        />
-
-        {/* Protected Home */}
-        <Route
-          path="/home"
-          element={isLoggedIn ? <Home /> : <Navigate to="/login" />}
-        />
-
-        {/* Fallback */}
-        <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   );
 }
