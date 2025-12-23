faeabb63fe773938d102950da36ea23bb4cd1d30
vigneshviki19 <vigneshvengatesh07@gmail.com>
2025-12-23 15:40:33 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -4,25 +4,18 @@ const api = axios.create({
   baseURL: "https://zingercat-backend.onrender.com/api",
 });
 
-export default api;
-
-export async function getPosts() {
-  const res = await api.get("/posts");
-  return res.data;
-}
-
-export async function createPost(content) {
-  const token = localStorage.getItem("token");
-
-  const res = await api.post(
-    "/posts",
-    { content },
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
+/**
+ * Automatically attach JWT token to every request
+ */
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-  );
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
-  return res.data;
-}
+export default api;
