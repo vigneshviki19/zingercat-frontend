6b372e4c464714e090cd9a57d81070e002566df3
vigneshviki19 <vigneshvengatesh07@gmail.com>
2025-12-28 13:30:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,76 +4,56 @@ import Landing from "./pages/Landing";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import EditProfile from "./pages/EditProfile";
 import Chat from "./pages/Chat";
 import PrivateChat from "./pages/PrivateChat";
 import Friends from "./pages/Friends";
 import FriendRequests from "./pages/FriendRequests";
 import Profile from "./pages/Profile";
-import EditProfile from "./pages/EditProfile";
 import Search from "./pages/Search";
 
 export default function App() {
   const token = localStorage.getItem("token");
+  const profileDone = localStorage.getItem("profileDone") === "true";
 
   return (
     <BrowserRouter>
       <Routes>
 
-        {/* ---------- PUBLIC ---------- */}
+        {/* PUBLIC */}
         <Route path="/" element={<Landing />} />
+        <Route path="/register" element={!token ? <Register /> : <Navigate to="/home" />} />
+        <Route path="/login" element={!token ? <Login /> : <Navigate to="/home" />} />
 
-        <Route
-          path="/register"
-          element={token ? <Navigate to="/home" /> : <Register />}
-        />
-
-        <Route
-          path="/login"
-          element={token ? <Navigate to="/home" /> : <Login />}
-        />
-
-        {/* ---------- PROTECTED ---------- */}
-        <Route
-          path="/home"
-          element={token ? <Home /> : <Navigate to="/login" />}
-        />
-
+        {/* PROFILE SETUP */}
         <Route
           path="/edit-profile"
-          element={token ? <EditProfile /> : <Navigate to="/login" />}
+          element={
+            token && !profileDone ? <EditProfile /> : <Navigate to="/home" />
+          }
         />
 
+        {/* HOME */}
         <Route
-          path="/chat"
-          element={token ? <Chat /> : <Navigate to="/login" />}
+          path="/home"
+          element={
+            token ? (
+              profileDone ? <Home /> : <Navigate to="/edit-profile" />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
         />
 
-        <Route
-          path="/chat/:user"
-          element={token ? <PrivateChat /> : <Navigate to="/login" />}
-        />
+        {/* OTHER PROTECTED */}
+        <Route path="/chat" element={token ? <Chat /> : <Navigate to="/login" />} />
+        <Route path="/chat/:user" element={token ? <PrivateChat /> : <Navigate to="/login" />} />
+        <Route path="/friends" element={token ? <Friends /> : <Navigate to="/login" />} />
+        <Route path="/requests" element={token ? <FriendRequests /> : <Navigate to="/login" />} />
+        <Route path="/profile/:username" element={token ? <Profile /> : <Navigate to="/login" />} />
+        <Route path="/search" element={token ? <Search /> : <Navigate to="/login" />} />
 
-        <Route
-          path="/friends"
-          element={token ? <Friends /> : <Navigate to="/login" />}
-        />
-
-        <Route
-          path="/requests"
-          element={token ? <FriendRequests /> : <Navigate to="/login" />}
-        />
-
-        <Route
-          path="/profile/:username"
-          element={token ? <Profile /> : <Navigate to="/login" />}
-        />
-
-        <Route
-          path="/search"
-          element={token ? <Search /> : <Navigate to="/login" />}
-        />
-
-        {/* ---------- FALLBACK ---------- */}
+        {/* FALLBACK */}
         <Route path="*" element={<Navigate to="/" />} />
 
       </Routes>
