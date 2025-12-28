6c8e62e6685cc2a5fdb921dadac09b32e60c5730
vigneshviki19 <vigneshvengatesh07@gmail.com>
2025-12-28 19:19:28 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: "https://zingercat-backend.onrender.com/api"
 });
 
-
+// attach token automatically
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
@@ -13,8 +13,7 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-/* ================= POSTS ================= */
-
+/* POSTS */
 export const getPosts = async () => {
   const res = await api.get("/posts");
   return res.data;
@@ -22,41 +21,6 @@ export const getPosts = async () => {
 
 export const createPost = async (content) => {
   const res = await api.post("/posts", { content });
-  return res.data;
-};
-
-/* ================= PROFILE ================= */
-
-export const getProfile = async (username) => {
-  const res = await api.get(`/profile/${username}`);
-  return res.data;
-};
-
-/* 🔍 SEARCH USERS (IMPORTANT FIX) */
-export const searchUsers = async (query) => {
-  const res = await api.get(`/profile/search?q=${query}`);
-  return res.data;
-};
-
-/* ================= FRIENDS ================= */
-
-export const sendFriendRequest = async (username) => {
-  const res = await api.post(`/friends/request/${username}`);
-  return res.data;
-};
-
-export const getFriends = async () => {
-  const res = await api.get("/friends");
-  return res.data;
-};
-
-export const getFriendRequests = async () => {
-  const res = await api.get("/friends/requests");
-  return res.data;
-};
-
-export const acceptFriend = async (username) => {
-  const res = await api.post(`/friends/accept/${username}`);
   return res.data;
 };
 
